f1c8a36b71fb6aead9d9bfce9f5e2bcb3f93fd97
Seth Lowie <slowie@exzeo.com>
2016-05-20 14:20:12 -0400

    custom pacakge.json on build

--- build.js
+++ build.js
@@ -1,8 +1,17 @@
-var copyfiles = require('copyfiles');
+// var copyfiles = require('copyfiles');
+//
+// copyfiles([
+//   './package.json',
+//   './bin',
+// ], true, function () {
+//   console.log('index.html copied to bin');
+// });
 
-copyfiles([
-  './package.json',
-  './bin',
-], true, function () {
-  console.log('index.html copied to bin');
-});
+var fs = require('fs');
+var package = require('./package.json');
+
+delete package.devDependencies;
+package.description = 'production npm to start built project';
+package.scripts = { start: 'node ./server' };
+
+fs.writeFile('bin/package.json', JSON.stringify(package));
