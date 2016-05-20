bc5468131987b3386212c80384c1281b17469cf4
Seth Lowie <slowie@exzeo.com>
2016-05-20 13:16:00 -0400

    build a server on jenkins

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.get('/', function (req, res) {
+  res.send('<h1>I am from the built server</h1>');
+});
 
 app.listen(PORT, function () {
   console.log('we hear you');
