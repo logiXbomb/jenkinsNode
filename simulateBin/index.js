d5dafa7f9879dda7a5a0e0007e43adf0355b7afd
Seth Lowie <slowie@exzeo.com>
2016-05-20 12:55:53 -0400

    simulated bin should show

---
+++ simulateBin/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, function () {
+  console.log('we hear you');
+});
